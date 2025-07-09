cf4b7bed445a63ef9b21fa9cd392f02c4fbb1232
Mario Galea <mario.galea84@gmail.com>
2025-07-09 13:26:18 +0200

    Start with Building the kafkaConsumer for email service to consume orders created

---
+++ email-service/KafkaConsumer.ts
@@ -0,0 +1,40 @@
+import { Kafka, Consumer } from 'kafkajs';
+import { Logger } from '../common/utils/Logger';
+
+export class KafkaConsumer {
+    private consumer: Consumer;
+    private topic: string;
+
+    constructor(broker: string, topic: string, groupId: string) {
+        const kafka = new Kafka({ brokers: [broker] });
+        this.consumer = kafka.consumer({ groupId });
+        this.topic = topic;
+    }
+
+    async connect() {
+        await this.consumer.connect();
+        Logger.debug(`Connected to Kafka as consumer for topic '${this.topic}'`);
+    }
+
+    async subscribe(onMessage: (message: any) => Promise<void>) {
+        Logger.debug(`Subscribing to topic '${this.topic}'`);
+
+        if (!this.consumer.connect()) {
+            Logger.warn(`Consumer is not connected. Attempting to connect to topic '${this.topic}'`);
+            await this.connect();
+        }
+
+        await this.consumer.subscribe({ topic: this.topic, fromBeginning: false });
+        Logger.debug(`Subscribed to topic '${this.topic}'`);
+        await this.consumer.run({
+            eachMessage: async ({ message }) => {
+                if (message.value) {
+                    Logger.debug(`Received message from topic '${this.topic}': ${message.value.toString()}`);
+                    // Parse the message value and pass it to the onMessage handler
+                    // Assuming the message value is a JSON string
+                    await onMessage(JSON.parse(message.value.toString()));
+                }
+            },
+        });
+    }
+}
